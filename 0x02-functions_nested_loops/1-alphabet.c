5ad63d073b1ca5d3858994ba58df3312cc0fc102
KHAOULITTAMEZ <ezzariba.khaoula89@gmail.com>
2023-03-21 14:33:37 -0700

    alphabeta

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,2 +1,20 @@
 #include<stdio.h>
 
+
+	/**
+	 * print_alphabet - print all alphabet in lowercase
+	 */
+
+
+	void print_alphabet(void)
+	{
+		char letter;
+
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+			putchar(letter);
+
+
+		putchar('\n');
+	}
+
