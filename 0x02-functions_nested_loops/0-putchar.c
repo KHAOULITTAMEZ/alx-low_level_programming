809b8a4176786fcdea3f9954adb8a0100236f947
KHAOULITTAMEZ <ezzariba.khaoula89@gmail.com>
2023-03-21 13:30:28 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
-/*
-* main - main  print _putchar
-* 
-* Return: 0
-*/
-int main(void)
+	/**
+	* main - main  print _putchar
+	* 
+	* Return: 0
+	*/
+	int main(void)
 	{
 	putchar('_');
 	putchar('p');
