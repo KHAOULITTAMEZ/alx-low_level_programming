e0bdb0436646de762b181a9e2612cd892acfa20e
KHAOULITTAMEZ <ezzariba.khaoula89@gmail.com>
2023-03-22 00:22:34 -0700

    put shar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,16 +8,16 @@
 	* Return: 0
 	*/
 
-	int main(void) 
+	int main(void)
 	{
-		putchar('_');
-		putchar('p');
-		putchar('u');
-		putchar('t');
-		putchar('c');
-		putchar('h');
-		putchar('a');
-		putchar('r');
-		putchar('\n');
-		return (0);
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
 	}
