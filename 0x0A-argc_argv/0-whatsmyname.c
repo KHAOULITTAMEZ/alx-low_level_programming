fbf83187354fbfaff64ddb47f3f2bc14f0068d40
KHAOULITTAMEZ <ezzariba.khaoula89@gmail.com>
2023-04-09 17:35:28 +0000

    task 1 of argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - whatsmyname print my name is myname
+ *
+ * return: value
+ */
+int main(int argc, char const *argv[])
+{ 
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}	
