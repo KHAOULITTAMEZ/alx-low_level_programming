f32e2a5093efa0907234b3e32bc2afa402d17dc1
KHAOULITTAMEZ <ezzariba.khaoula89@gmail.com>
2023-04-09 06:39:30 -0700

    static libraries

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,8 +1,14 @@
 #include "main.h"
 
+/**
+ *main  - main.c for executing static libraries
+ *
+ * Return: 0 if anything is good
+ */
+
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- 
-	Kevin Mitnick");
+	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t-
+			Kevin Mitnick");
 	return (0);
 }
