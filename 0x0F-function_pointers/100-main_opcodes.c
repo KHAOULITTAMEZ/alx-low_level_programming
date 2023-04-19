c94d05032fc15ba0a315da82ef8540edde8a4784
KHAOULITTAMEZ <ezzariba.khaoula89@gmail.com>
2023-04-19 16:29:06 -0700

    opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -25,12 +25,12 @@ int main(int argc, char *argv[])
 	}
 	arr = (char *)main;
 	for (i = 0; i < bytes; i++)
-		{
-		if (i == bytes - 1)
-		{
+	{
+	if (i == bytes - 1)
+	{
 		printf("%02hhx\n", arr[i]);
 		break;
-		}
+	}
 		printf("%02hhx ", arr[i]);
 	}
 	return (0);
